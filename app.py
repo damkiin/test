09a970050d2a64305e341d330216f8976ed96438
damkiin <65850524+damkiin@users.noreply.github.com>
2020-05-30 20:06:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -20,9 +20,30 @@ data_count.index = pd.DatetimeIndex(data_count.index)
 date_new = pd.date_range(start = '2020-01-01', end =datetime.date.today())
 data_count = data_count.reindex(date_new, fill_value = 0)
 
-
+#Edits fun dash app
+text1= '  DISCLAIMER: This is a visualization of a public dataset made available by the Government of Ontario. The dataset used in creating ths dashboard is available at: '
+urll ='  https://data.ontario.ca/dataset/confirmed-positive-cases-of-covid-19-in-ontario/resource/455fd63b-603d-4608-8216-7d8647f43350'
+text2='  The dashboard has not been created to compare the efficiency of public health units and has been deliberatly designed to discourage this. Every health unit is doing the absolute best that can be done given the resources available. Keep supporting your health unit and stay safe'
+text3= 'For suggestions contact @DAMKIIN on twitter'
 
 option = [{'label': phus, 'value': phus} for phus in data_raw['Reporting_PHU'].unique()]
+
+#default graphs with texts for eight columns
+layout_default = go.Layout(dragmode= False, annotations = [
+    go.layout.Annotation(
+        text = 'SELECT A <BR> PUBLIC HEALTH UNIT TO <BR> POPULATE THE FIGURE',
+        showarrow=False,
+        font= dict(size= 40)
+    )])
+figur = go.Figure(layout = layout_default)
+#default graphs with texts for four columns
+layout_default1 = go.Layout(dragmode= False, annotations = [
+    go.layout.Annotation(
+        text = 'SELECT A <BR> PUBLIC HEALTH UNIT TO <BR> POPULATE THE FIGURE',
+        showarrow=False,
+        font= dict(size= 30)
+    )])
+figur1 = go.Figure(layout = layout_default1)
 
 #The dash app gaangaan
 app = dash.Dash(external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
@@ -30,37 +51,49 @@ server = app.server
 
 app.layout = html.Div([
     html.Div(html.H1('COVID-19 TREND (ONTARIO HEALTH UNITS)', style=dict(textAlign='center')), className="row"),
-    html.Div(['Awon temi shey wa o?'], className="row"),
-    html.Div(html.Label('Select health unit'), style=dict(color='blue', textAlign='center')),
+    html.Div(html.Label('Select a health unit from the dropdown menu below'),
+             style=dict(color='blue', textAlign='center')),
     html.Div([dcc.Dropdown(
-        id='demo-dropdown',
+        id='dropdown',
         options=option,
         value='',
         style=dict(width='50%', fontweight='bold', margin='auto', textAlign='center')
     )], className="row"),
     html.Div([
-        html.Div([dcc.Graph(id='graph1')], className="eight columns"),
-        html.Div([dcc.Graph(id='graph2')], className="four columns"),
+        html.Div([dcc.Graph(id='graph1', figure=figur)], className="eight columns"),
+        html.Div([dcc.Graph(id='graph2', figure=figur1)], className="four columns"),
     ], className="row"),
     html.Div([
-        html.Div([dcc.Graph(id='graph6')], className="four columns"),
-        html.Div([dcc.Graph(id='graph7')], className="eight columns"),
+        html.Div([dcc.Graph(id='graph6', figure=figur1)], className="four columns"),
+        html.Div([dcc.Graph(id='graph7', figure=figur)], className="eight columns"),
     ], className="row"),
     html.Div([
-        html.Div([dcc.Graph(id='graph3')], className="four columns"),
-        html.Div([dcc.Graph(id='graph4')], className="four columns"),
-        html.Div([dcc.Graph(id='graph5')], className="four columns"),
+        html.Div([dcc.Graph(id='graph3', figure=figur1)], className="four columns"),
+        html.Div([dcc.Graph(id='graph4', figure=figur1)], className="four columns"),
+        html.Div([dcc.Graph(id='graph5', figure=figur1)], className="four columns"),
     ], className="row"),
+    html.Div(['.']),
 
-    html.Div(['row5'], className="row"),
-    html.Div(['row6'], className="row"),
-    html.Div(['row7'], className="row"),
-    html.Div(['row8'], className="row"),
+    html.Div([
+        html.Div([]),
+        html.Article([]),
+        html.Div([
+            html.Div([
+                html.Article(text1),
+                dcc.Link(urll, href=urll),
+                html.Article([text2]),
+                html.Article(text3)
+            ]),
+
+        ]),
+    ], style=dict(border='0.5px black solid', Align='right', width='50%')),
+    html.Div(['.']),
+    html.Div(['.']),
+
 ])
 
-
 @app.callback(dash.dependencies.Output('graph1', 'figure'),
-              [dash.dependencies.Input('demo-dropdown', 'value')])
+              [dash.dependencies.Input('dropdown', 'value')])
 def graph1(phu):
     y_count = data_count[phu]
     y_cumul = data_count[phu].cumsum()
@@ -81,67 +114,67 @@ def graph1(phu):
 
 @app.callback(
     dash.dependencies.Output('graph2', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph2(phu):
     x = data_raw.groupby(by='Reporting_PHU').get_group(phu)
     y = 'Outcome1'
     data1 = go.Pie(labels=pd.value_counts(x[y]).index, values=pd.value_counts(x[y]), hole=.5, textinfo='label+percent')
-    layout1 = go.Layout(title=y, showlegend=False)
+    layout1 = go.Layout(title='Case Status', showlegend=False)
     fig2 = go.Figure(data=data1, layout=layout1)
     return fig2
 
 
 @app.callback(
     dash.dependencies.Output('graph3', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph3(phu):
     x = data_raw.groupby(by='Reporting_PHU').get_group(phu)
     y = 'Client_Gender'
     data1 = go.Pie(labels=pd.value_counts(x[y]).index, values=pd.value_counts(x[y]), hole=.5, textinfo='label+percent')
-    layout1 = go.Layout(title=y, showlegend=False)
+    layout1 = go.Layout(title= 'Gender Distribution', showlegend=False)
     fig3 = go.Figure(data=data1, layout=layout1)
     return fig3
 
 
 @app.callback(
     dash.dependencies.Output('graph4', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph4(phu):
     x = data_raw.groupby(by='Reporting_PHU').get_group(phu)
     y = 'Age_Group'
     data1 = go.Pie(labels=pd.value_counts(x[y]).index, values=pd.value_counts(x[y]), hole=.5, textinfo='label+percent')
-    layout1 = go.Layout(title=y, showlegend=False)
+    layout1 = go.Layout(title='Age Distribution', showlegend=False)
     fig4 = go.Figure(data=data1, layout=layout1)
     return fig4
 
 
 @app.callback(
     dash.dependencies.Output('graph5', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph5(phu):
     x = data_raw.groupby(by='Reporting_PHU').get_group(phu)
     y = 'Case_AcquisitionInfo'
     data1 = go.Pie(labels=pd.value_counts(x[y]).index, values=pd.value_counts(x[y]), hole=.5, textinfo='label+percent')
-    layout1 = go.Layout(title=y, showlegend=False)
+    layout1 = go.Layout(title='Case transmission information', showlegend=False)
     fig5 = go.Figure(data=data1, layout=layout1)
     return fig5
 
 
 @app.callback(
     dash.dependencies.Output('graph6', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph6(phu):
     x = data_raw.groupby(by='Reporting_PHU').get_group(phu)
     y = 'Outbreak_Related'
     data1 = go.Pie(labels=pd.value_counts(x[y]).index, values=pd.value_counts(x[y]), hole=.5, textinfo='label+percent')
-    layout1 = go.Layout(title=y, showlegend=False)
+    layout1 = go.Layout(title='Proportion of Cases that are outbreak related', showlegend=False)
     fig6 = go.Figure(data=data1, layout=layout1)
     return fig6
 
 
 @app.callback(
     dash.dependencies.Output('graph7', 'figure'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
+    [dash.dependencies.Input('dropdown', 'value')])
 def graph8(phu):
     y_rolling_avg_14 = data_count[phu].rolling(14).mean()
     y_cumul = data_count[phu].cumsum()
